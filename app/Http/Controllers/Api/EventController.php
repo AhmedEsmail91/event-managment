36dc2b4d98c503b4da035980b7601d938f3e29ca
Ahmed Esmail <107895995+AhmedEsmail91@users.noreply.github.com>
2024-02-19 20:03:09 +0200

    CRUD

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -13,6 +15,7 @@ class EventController extends Controller
     public function index()
     {
         //
+        return Event::all();
     }
 
     /**
@@ -21,29 +24,67 @@ class EventController extends Controller
     public function store(Request $request)
     {
         //
+        $request->validate([
+            "name"=> "required|string|max:255",
+            'description' => 'nullable|string',
+            // we can specify the format of the data as a condition as following |date_format:Y-m-d H:i:s|
+            'start_time'=>"required|date",
+            'end_time'=>'required|date|after:start_time'
+        ]);
+        // this changes must be done after learning about authentication but since we have already users so lets continue.
+
+        $event = Event::create([
+            ...$request->validate // the ... means the spread operator to copy all request elements in the the outer array cause we need in that moment to add the user id seperatlly 
+                ([
+                    "name"=> "required|string|max:255",
+                    'description' => 'nullable|string',
+                    // we can specify the format of the data as a condition as following |date_format:Y-m-d H:i:s|
+                    'start_time'=>"required|date",
+                    'end_time'=>'required|date|after:start_time'
+                ]),
+                'user_id'=>1
+        ]);
+        // $event = Event::create($request->all());
+        return $event;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event)
     {
-        //
+        return $event;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+        $event->update([
+            ...$request->validate 
+            ([
+                "name"=> "sometimes|string|max:255",
+                'description' => 'nullable|string',
+                
+                'start_time'=>"sometimes|date",
+                'end_time'=>'sometimes|date|after:start_time'
+            ])
+            ]);
+        return $event;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
-        //
+        $event->delete();
+        return response()->json(['message'=>'Event deleted successfully']);
+        // Or just  do this if you don't need to send a message back
+        // return response(status:204);
+        // Or
+        // return response('',204);
+        // return redirect()->route('events.index');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,31 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-// only register the routes which are responsible for listing, showing one resource and can adding modifing it without the route of forms
 
-Route::apiResource('user',EventController::class);
-/* while attendees  are related to events, we don't need a separate route for that */
+Route::apiResource('events',EventController::class);
+
+
 Route::apiResource('events.attendees', AttendeeController::class)
-    ->scoped(['attendee'=>'event']) // this means that the attendee resource s are always part of an event,
-    //also means that if you would use route model binding to get an attendee,
-    //Laravel will automatically loaded by looking for the attendees of a parent event
-;
-
-/*
-We Defining for the first time the routes in the api.php file not web.php, here's why?:
-    
-    - The api is more focused on data transfer between client and server than presentation.
-    So it doesn't really make sense to have views associated with these routes.
-    
-    - In our case, there are no users who should see or interact with these data, only the system itself.
-    This makes it easier to keep everything clean and simple.
-    
-    - We want to showcase how to structure an API as best as possible.
-    
-    - In the RouteServiceProvider you can see that each api route is prefixed with 'api'.
-    This means that all routes in this file are under that prefix.
-    If you wanted to add another set of routes without the prefix, you could do so by adding another instance of Route::prefix()
-    You could also add other things like versioning, authentication etc. to the prefix.
-    This means that each file has its own middleware groups in the (Kernel) which can then easily be added / removed from the entire api.
-
-*/
+    ->scoped(['attendee'=>'event']);
